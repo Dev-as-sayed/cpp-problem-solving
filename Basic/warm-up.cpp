6ac4774413a59be9d66a877ebde2957a4f72b561
wazed-md-abdul <assayed277@gmail.com>
2026-03-23 07:19:43 +0600

    first commit

---
+++ Basic/warm-up.cpp
@@ -0,0 +1,59 @@
+/**
+ * ==================================================
+ * 1. Even or Odd
+ *  Input: integer n
+ *  Output: "Even" or "Odd"
+ * ==================================================
+*/
+
+
+
+#include<iostream>
+
+int main() {
+
+    int oddIntup;
+
+    std::cout << "give me a ODD number " << std::endl;
+    std:: cin >> oddIntup ;
+    std:: cout << "varifying " << oddIntup << " is odd or not....."<<std::endl;
+
+    if (oddIntup % 2 != 0){
+        std::cout << "your provided number is EVEN"<<std::endl; 
+    }else{
+        std::cout << "your provided number is ODD"<<std::endl; 
+    }
+    
+    return 0;
+}
+
+
+/**
+ * 2. Positive, Negative, or Zero
+ *  Classify the number
+ * ==================================================
+ */
+
+/**
+ * ==================================================
+ * 3. Largest of Two Numbers
+ *  Input: a, b
+ * ==================================================
+ */
+
+
+
+/**
+ * ==================================================
+ * 4. Largest of Three Numbers
+ *  Use nested conditions
+ * ==================================================
+ */
+
+/**
+ * ==================================================
+ * 5. Swap Two Numbers
+ *  Without using a third variable
+ * ==================================================
+*/
+
